ede997eb9f9f618c8c19bb6874d511e3f48b7aa0
fernandotresa <41007445+fernandotresa@users.noreply.github.com>
2018-07-09 20:09:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,18 +4,32 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override')
 var cors = require('cors');
+var fs = require("fs");
 var app = express();
+var shell = require('shelljs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(cors());
 
+
+var admin = require("firebase-admin");
+var serviceAccount = require('./serviceAccountKey.json');
+var firebase = require('firebase-admin');
+var client = 'uifhsh34ud2'        
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://totem-beba5.firebaseio.com"
+});
+
+var shell = require('shelljs');
+
 var con = mysql.createConnection({
     host: "10.0.2.180",
     user: "root",
-    password: "Mudaragora00",
-    database: 'zoologico'
+    password: "Mudaragora00"
  });
 
  con.connect(function(err) {
@@ -30,37 +44,34 @@ function log_(str){
         console.log(str)
 }
 
-app.post('/getVisitantes', function(req, res) {
+
+function dataAtualFormatada(){
+    var data = new Date();
+    var dia = data.getDate();
+    if (dia.toString().length == 1)
+      dia = "0"+dia;
+    var mes = data.getMonth()+1;
+    if (mes.toString().length == 1)
+      mes = "0"+mes;
+    var ano = data.getFullYear();  
+    return dia+"/"+mes+"/"+ano;
+}
+
+app.post('/getAreas', function(req, res) {
 
     var idTotem = req.body.id
 
     log_('Totem: '+ idTotem + ' - Verificando todas as areas:')
             
     var sql = "SELECT \
-        visitantes.id,\
-        UPPER(visitantes.name) AS name,\
-        UPPER(visitantes.cpf) AS cpf,\
-        UPPER(visitantes.rg) AS rg,\
-        UPPER(visitantes.telefone) AS telefone,\
-        UPPER(visitantes.endereco) AS endereco,\
-        UPPER(visitantes.bairro) AS bairro,\
-        UPPER(visitantes.obs) AS obs,\
-        visitantes.fotosamba,\
-        visitantes.status,\
-        crachas.id AS CRACHA_ID,\
-        crachas.id_tipo AS CRACHA_TIPO,\
-        crachas.id_cracha AS CRACHA,\
-        UPPER(funcionarios.name) AS AUTORIZANTE,\
-        funcionarios.id AS AUTORIZANTE_ID,\
-        visitantes_tipos.id AS id_tipo,\
-        visitantes_tipos.name AS TIPO,\
-        UPPER(empresas.name) AS EMPRESA \
-    FROM visitantes \
-    LEFT JOIN  visitantes_tipos ON visitantes_tipos.id =  visitantes.id_tipo \
-    LEFT JOIN  funcionarios ON funcionarios.id =  visitantes.id_autorizado_por \
-    LEFT JOIN  crachas ON crachas.id =  visitantes.id_cracha \
-    LEFT JOIN  empresas ON empresas.id =  visitantes.id_empresa \
-    WHERE visitantes.status = 1;";
+    3access.3a_area_acesso.id_area_acesso,\
+    3access.3a_area_acesso.nome_area_acesso,\
+    3access.3a_area_acesso.lotacao_area_acesso,\
+    3access.3a_area_acesso.ativo \
+    FROM \
+    3access.3a_area_acesso";
+
+    //log_(sql)
 
     con.query(sql, function (err1, result) {        
         if (err1) throw err1;           
@@ -68,38 +79,23 @@ app.post('/getVisitantes', function(req, res) {
     });
 });
 
-app.post('/getVisitantesNome', function(req, res) {
+app.post('/getAreaCounter', function(req, res) {
 
     var idTotem = req.body.id
     var idArea = req.body.idArea    
 
     log_('Totem: '+ idTotem + ' - Verificando contador da area:', idArea)
             
-    let sql = "SELECT \
-        visitantes.id,\
-        UPPER(visitantes.name) AS name,\
-        UPPER(visitantes.cpf) AS cpf,\
-        UPPER(visitantes.rg) AS rg,\
-        UPPER(visitantes.telefone) AS telefone,\
-        UPPER(visitantes.endereco) AS endereco,\
-        UPPER(visitantes.bairro) AS bairro,\
-        UPPER(visitantes.obs) AS obs,\
-        visitantes.foto,\
-        visitantes.fotosamba,\
-        visitantes.status,\
-        crachas.id AS CRACHA_ID,\
-        crachas.id_tipo AS CRACHA_TIPO,\
-        crachas.id_cracha AS CRACHA,\
-        funcionarios.id AS AUTORIZANTE_ID,\
-        UPPER(funcionarios.name) AS AUTORIZANTE,\
-        UPPER(visitantes_tipos.name) AS TIPO,\
-        UPPER(empresas.name) AS EMPRESA \
-    FROM visitantes \
-    LEFT JOIN  visitantes_tipos ON visitantes_tipos.id =  visitantes.id_tipo \
-    LEFT JOIN  funcionarios ON funcionarios.id =  visitantes.id_autorizado_por \
-    LEFT JOIN  crachas ON crachas.id =  visitantes.id_cracha \
-    LEFT JOIN  empresas ON empresas.id =  visitantes.id_empresa \
-    WHERE visitantes.name LIKE '%%1%' AND visitantes.status = %2;"
+    var sql = "SELECT \
+    3access.3a_area_acesso.id_area_acesso,\
+    3access.3a_area_acesso.nome_area_acesso,\
+    3access.3a_area_acesso.lotacao_area_acesso,\
+    3access.3a_area_acesso.ativo \
+    FROM \
+    3access.3a_area_acesso \
+    WHERE 3access.3a_area_acesso.id_area_acesso = " + idArea + ";"
+
+    //log_(sql)
 
     con.query(sql, function (err1, result) {        
         if (err1) throw err1;           
@@ -120,6 +116,25 @@ app.post('/decrementAreaCounter', function(req, res) {
     WHERE 3access.3a_area_acesso.id_area_acesso = " + idArea + ";"
 
    // log_(sql)
+
+    con.query(sql, function (err1, result) {        
+        if (err1) throw err1;           
+        res.json({"success": result}); 
+    });
+});
+
+app.post('/checkTicket', function(req, res) {
+
+    var idTotem = req.body.id
+    var ticket = req.body.ticket
+
+    log_('Totem: '+ idTotem + ' - Verificando ticket:', ticket)
+            
+    var sql = "SELECT 3access.3a_log_vendas.* \
+    FROM 3access.3a_log_vendas \
+    WHERE 3access.3a_log_vendas.fk_id_estoque_utilizavel = " + ticket + ";"
+
+   log_(sql)
 
     con.query(sql, function (err1, result) {        
         if (err1) throw err1;           
