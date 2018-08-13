0ea1cae15f0fce138d3cfeb87f7a727e7ec10a49
Diego Queiroz dos Santos <diego.freebsd@gmail.com>
2018-08-13 09:07:30 -0300

    Erro no fluxo

--- index.js
+++ index.js
@@ -313,6 +313,23 @@ app.post('/checkTicketIsSold', function(req, res) {
     });    
 });
 
+app.post('/checkTicketExist', function(req, res) {
+
+    let idTotem = req.body.id    
+    let ticket = req.body.ticket
+
+    log_('Totem: '+ idTotem + ' - Verificando se ticket existe:', ticket)
+            
+    let sql = "SELECT * FROM 3a_estoque_utilizavel WHERE 3a_estoque_utilizavel.id_estoque_utilizavel = " + ticket + ";"
+
+   log_(sql)
+
+    con.query(sql, function (err1, result) {        
+        if (err1) throw err1;           
+        res.json({"success": result}); 
+    });
+});
+
 app.post('/checkTicket', function(req, res) {
 
     let idTotem = req.body.id
@@ -347,7 +364,7 @@ app.post('/checkTicketContinue', function(req, res) {
     let idPorta = req.body.idPorta
     let idArea = req.body.idArea
 
-    log_('Totem: '+ idTotem + ' - Verificando ticket:', ticket)
+    log_('Totem: '+ idTotem + ' - Verificando ticket continuação:', ticket)
             
     let sql = "SELECT 3a_log_vendas.data_log_venda,\
         3a_estoque_utilizavel.id_estoque_utilizavel,\
@@ -363,8 +380,8 @@ app.post('/checkTicketContinue', function(req, res) {
     INNER JOIN 3a_estoque_utilizavel ON 3a_estoque_utilizavel.id_estoque_utilizavel = 3a_log_vendas.fk_id_estoque_utilizavel \
     INNER JOIN 3a_validade ON 3a_validade.id_validade = 3a_log_vendas.fk_id_validade \
     INNER join 3a_subtipo_area_autorizada ON 3a_subtipo_area_autorizada.fk_id_subtipo = 3a_subtipo_produto.id_subtipo_produto \
-    INNER JOIN 3a_porta_acesso ON 3a_porta_acesso.fk_id_area_acesso = 3a_subtipo_area_autorizada.fk_id_area_acesso \
-    INNER JOIN 3a_area_acesso ON 3a_area_acesso.id_area_acesso = 3a_porta_acesso.fk_id_area_acesso \
+    INNER JOIN 3a_area_acesso ON 3a_area_acesso.id_area_acesso = 3a_subtipo_area_autorizada.fk_id_area_acesso \
+    INNER JOIN 3a_porta_acesso ON 3a_porta_acesso.fk_id_area_acesso = 3a_area_acesso.id_area_acesso \
     WHERE 3a_estoque_utilizavel.id_estoque_utilizavel = " + ticket + "\
     AND 3a_porta_acesso.id_porta_acesso = " + idPorta + "\
     AND 3a_subtipo_area_autorizada.fk_id_area_acesso = " + idArea + ";";
